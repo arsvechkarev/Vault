091c8e6c0ebe0cc4e1602971549a34e978142129
arsvechkarev <arsvechkarev@gmail.com>
2021-04-21 16:22:32 +0300

    added test comment

--- app/src/main/java/com/arsvechkarev/vault/features/initial_screen/InitialScreen.kt
+++ app/src/main/java/com/arsvechkarev/vault/features/initial_screen/InitialScreen.kt
@@ -24,7 +24,8 @@ import com.arsvechkarev.vault.viewdsl.text
 import com.arsvechkarev.vault.viewdsl.textSize
 
 class InitialScreen : ViewScreen() {
-  
+
+  // whatever
   override fun buildLayout() = withViewBuilder {
     RootFrameLayout {
       VerticalLayout(MatchParent, WrapContent) {

383118e8d258ecd6d98ba23baf10647ebc2faef7
arsvechkarev <arsvechkarev@gmail.com>
2021-05-07 14:09:31 +0300

    Added channel classes

---
+++ app/src/main/java/com/arsvechkarev/vault/core/channels/BehaviorChannel.kt
@@ -0,0 +1,25 @@
+package com.arsvechkarev.vault.core.channels
+
+/**
+ * Channel that behaves like BehaviorSubject in RxJava: when new subscriber is added, it receives
+ * latest event if there was one
+ */
+class BehaviorChannel<E> : Channel<E> {
+  
+  private val subscribers = ArrayList<(E) -> Unit>()
+  private var latestEvent: E? = null
+  
+  override fun send(event: E) {
+    latestEvent = event
+    subscribers.forEach { it.invoke(event) }
+  }
+  
+  override fun subscribe(listener: (E) -> Unit) {
+    subscribers.add(listener)
+    latestEvent?.let(listener::invoke)
+  }
+  
+  override fun unsubscribe(listener: (E) -> Unit) {
+    subscribers.remove(listener)
+  }
+}

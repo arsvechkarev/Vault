7dcb5ffa8f2bd41825cbaa55132f4f196bdf32f1
arsvechkarev <arsvechkarev@gmail.com>
2022-08-12 15:10:24 +0300

    Probably fixed bug with store

--- android-app/app/src/main/java/com/arsvechkarev/vault/core/mvi/tea/TeaStoreImpl.kt
+++ android-app/app/src/main/java/com/arsvechkarev/vault/core/mvi/tea/TeaStoreImpl.kt
@@ -3,17 +3,12 @@ package com.arsvechkarev.vault.core.mvi.tea
 import android.util.Log
 import com.arsvechkarev.vault.core.DispatchersFacade
 import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.onSubscription
-import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.util.concurrent.atomic.AtomicInteger
 
 class TeaStoreImpl<State : Any, Event : Any, UiEvent : Event, Command : Any, News : Any>(
   private val actors: List<Actor<Command, Event>>,
@@ -21,9 +16,10 @@ class TeaStoreImpl<State : Any, Event : Any, UiEvent : Event, Command : Any, New
   initialState: State,
 ) : TeaStore<State, UiEvent, News> {
   
-  private val commandsFlow = MutableSharedFlow<Command>()
+  private val commandsFlow = MutableSharedFlow<Command>(replay = 1)
   
   private val eventsFlow = MutableSharedFlow<Event>(
+    replay = 1,
     extraBufferCapacity = 1,
     onBufferOverflow = BufferOverflow.DROP_OLDEST,
   )
@@ -31,54 +27,40 @@ class TeaStoreImpl<State : Any, Event : Any, UiEvent : Event, Command : Any, New
   override val state = MutableStateFlow(initialState)
   override val news = MutableSharedFlow<News>()
   
-  private val actorsLatch = CompletableDeferred<Unit>()
-  private val eventsLatch = CompletableDeferred<Unit>()
-  
-  private val counter = AtomicInteger(actors.size)
-  
   override fun launch(coroutineScope: CoroutineScope, dispatchersFacade: DispatchersFacade) {
     actors.forEach { actor ->
       coroutineScope.launch(dispatchersFacade.IO) {
         try {
           actor.handle(commandsFlow)
-              .shareIn(coroutineScope, started = WhileSubscribed(replayExpirationMillis = 0))
-              .onSubscription {
-                if (counter.decrementAndGet() == 0) {
-                  actorsLatch.complete(Unit)
-                }
-              }
               .collect(eventsFlow::emit)
         } catch (e: CancellationException) {
-          Log.d("TeaStoreImpl", "error, throwing $e")
+          Log.d("TeaStoreImpl", "error, throwing $e", e)
           throw e
         }
       }
     }
     coroutineScope.launch(dispatchersFacade.Default) {
-      eventsFlow.onSubscription { eventsLatch.complete(Unit) }
-          .collect { event ->
-            val currentState = state.value
-            val update = reducer.reduce(currentState, event)
-            if (update.state != currentState) {
-              withContext(dispatchersFacade.Main) {
-                state.emit(update.state)
-              }
-            }
-            update.commands.forEach { command ->
-              commandsFlow.emit(command)
-            }
-            withContext(dispatchersFacade.Main) {
-              update.news.forEach { newsItem ->
-                news.emit(newsItem)
-              }
-            }
+      eventsFlow.collect { event ->
+        val currentState = state.value
+        val update = reducer.reduce(currentState, event)
+        if (update.state != currentState) {
+          withContext(dispatchersFacade.Main) {
+            state.emit(update.state)
           }
+        }
+        update.commands.forEach { command ->
+          commandsFlow.emit(command)
+        }
+        withContext(dispatchersFacade.Main) {
+          update.news.forEach { newsItem ->
+            news.emit(newsItem)
+          }
+        }
+      }
     }
   }
   
   override suspend fun dispatch(event: UiEvent) {
-    actorsLatch.await()
-    eventsLatch.await()
     eventsFlow.emit(event)
   }
   

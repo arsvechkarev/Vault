e107a5d66d34a849cb3d36f6d03dd6cd928c5f4e
a.svechkarev <a.svechkarev@tinkoff.ru>
2022-11-25 14:43:05 +0200

    Removed teako

--- android-app/kotea/src/main/java/kotea/impl/CommandsFlowHandlerException.kt
+++
@@ -1,8 +0,0 @@
-package kotea.impl
-
-import kotea.CommandsFlowHandler
-
-internal class CommandsFlowHandlerException(
-    handlerClass: Class<out CommandsFlowHandler<*, *>>,
-    cause: Throwable
-) : RuntimeException("Exception in ${handlerClass.canonicalName}", cause)

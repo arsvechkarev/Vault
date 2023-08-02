7a7adcbd2fda2ec1facb32b8b7c98d62fc39ecd4
arsvechkarev <arsvechkarev@gmail.com>
2023-08-02 23:46:26 +0300

    Implemented decryption logic

--- desktop-app/src/main/kotlin/com/arsvechkarev/vaultdesktop/controllers/LoginController.kt
+++ desktop-app/src/main/kotlin/com/arsvechkarev/vaultdesktop/controllers/LoginController.kt
@@ -1,8 +1,15 @@
 package com.arsvechkarev.vaultdesktop.controllers
 
 import com.arsvechkarev.commoncrypto.AesSivTinkCipher
+import com.arsvechkarev.vaultdesktop.style.Colors
 import javafx.fxml.FXML
 import javafx.scene.control.Label
+import javafx.scene.control.PasswordField
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyEvent
+import javafx.scene.paint.Paint
+import javafx.stage.FileChooser
+import java.io.File
 
 class LoginController {
   
@@ -16,15 +23,54 @@ class LoginController {
   private lateinit var masterPasswordLabel: Label
   
   @FXML
+  private lateinit var masterPasswordField: PasswordField
+  
+  private var file: File? = null
+  
+  @FXML
   fun onOpenFileClick() {
-    val encrypt = AesSivTinkCipher.encrypt("qwertt", "ddfsdf")
-    val decrypt = AesSivTinkCipher.decrypt("qwertt", encrypt)
+    filenameHeader.text = "File"
+    filenameHeader.textFill = Paint.valueOf(Colors.ACCENT)
+    file = FileChooser().apply { title = "Select passwords vault" }
+        .showOpenDialog(filenameLabel.scene.window) ?: return
+    filenameLabel.text = file!!.absolutePath
+  }
+  
+  @FXML
+  fun onKeyPressed(event: KeyEvent) {
+    if (event.code == KeyCode.ENTER) {
+      onContinueClick()
+    } else {
+      masterPasswordLabel.text = "Master password"
+      masterPasswordLabel.textFill = Paint.valueOf(Colors.ACCENT)
+    }
   }
   
   @FXML
   fun onContinueClick() {
-    filenameLabel.text = "test"
-    val encrypt = AesSivTinkCipher.encrypt("qwertt", "ddfsdf")
-    val decrypt = AesSivTinkCipher.decrypt("qwertt", encrypt)
+    val file = file
+    if (file == null) {
+      filenameHeader.text = "File is not selected"
+      filenameHeader.textFill = Paint.valueOf(Colors.ERROR)
+      return
+    }
+    val password = masterPasswordField.text
+    if (password.isNullOrBlank()) {
+      masterPasswordLabel.text = "Password is empty"
+      masterPasswordLabel.textFill = Paint.valueOf(Colors.ERROR)
+      return
+    }
+    
+    val data = file.readBytes()
+    runCatching { AesSivTinkCipher.decrypt(password, data) }
+        .onFailure {
+          masterPasswordLabel.text = "Password is incorrect"
+          masterPasswordLabel.textFill = Paint.valueOf(Colors.ERROR)
+        }
+        .onSuccess {
+          println("================================")
+          println(it)
+          println("================================")
+        }
   }
 }

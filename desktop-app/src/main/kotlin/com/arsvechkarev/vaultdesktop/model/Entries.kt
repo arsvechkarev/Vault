53679f0d5596c2131f7d4cee902a076157d0eeed
arsvechkarev <arsvechkarev@gmail.com>
2023-08-03 23:24:39 +0300

    Added read-only displaying of data

---
+++ desktop-app/src/main/kotlin/com/arsvechkarev/vaultdesktop/model/Entries.kt
@@ -0,0 +1,12 @@
+package com.arsvechkarev.vaultdesktop.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Entries(
+  @SerializedName("passwords")
+  val passwords: List<Password>,
+  @SerializedName("creditCards")
+  val creditCards: List<CreditCard>,
+  @SerializedName("plainTexts")
+  val plainTexts: List<PlainText>
+)

5b5c6513edbc92072fa5e07deb5eeba075ac0f2d
arsvechkarev <arsvechkarev@protonmail.com>
2023-08-06 00:06:36 +0300

    Fixed LetterInCircleDrawable text size caclculation

--- android-app/app/src/main/java/com/arsvechkarev/vault/core/views/drawables/LetterInCircleDrawable.kt
+++ android-app/app/src/main/java/com/arsvechkarev/vault/core/views/drawables/LetterInCircleDrawable.kt
@@ -5,10 +5,8 @@ import android.graphics.Paint
 import android.graphics.Rect
 import com.arsvechkarev.vault.core.extensions.Paint
 import com.arsvechkarev.vault.core.extensions.TextPaint
-import com.arsvechkarev.vault.core.extensions.getTextHeight
 import com.arsvechkarev.vault.viewbuilding.Colors
 import com.arsvechkarev.vault.viewbuilding.Fonts
-import com.arsvechkarev.vault.viewbuilding.TextSizes
 
 class LetterInCircleDrawable(
   private var letter: String,
@@ -34,28 +32,14 @@ class LetterInCircleDrawable(
     invalidateSelf()
   }
   
-  override fun onBoundsChange(bounds: Rect) {
-    computeTextSize()
-    textPaint.getTextBounds(letter, 0, letter.length, textRect)
-  }
-  
   override fun draw(canvas: Canvas) {
     val hw = bounds.width() / 2f
     val hh = bounds.height() / 2f
+    textPaint.textSize = minOf(hw, hh)
+    textPaint.getTextBounds(letter, 0, letter.length, textRect)
     canvas.drawCircle(hw, hh, minOf(hw, hh), paint)
     val x = hw - textRect.width() / 2f - textRect.left
     val y = hh + textRect.height() / 2f - textRect.bottom
     canvas.drawText(letter, x, y, textPaint)
-  }
-  
-  private fun computeTextSize() {
-    textPaint.textSize = TextSizes.H4
-    while (true) {
-      val textHeight = textPaint.getTextHeight(letter)
-      if (textHeight > bounds.height() * 0.4f) {
-        break
-      }
-      textPaint.textSize++
-    }
   }
 }

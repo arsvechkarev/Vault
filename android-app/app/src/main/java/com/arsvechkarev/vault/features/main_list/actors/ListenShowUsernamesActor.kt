8260f71bc097e53fcacc257981bcde1a6defa2aa
arsvechkarev <arsvechkarev@protonmail.com>
2023-09-30 01:07:12 +0400

    Implemented displaying of usernames

---
+++ android-app/app/src/main/java/com/arsvechkarev/vault/features/main_list/actors/ListenShowUsernamesActor.kt
@@ -0,0 +1,21 @@
+package com.arsvechkarev.vault.features.main_list.actors
+
+import com.arsvechkarev.vault.core.mvi.tea.Actor
+import com.arsvechkarev.vault.features.common.domain.ShowUsernamesInteractor
+import com.arsvechkarev.vault.features.main_list.MainListCommand
+import com.arsvechkarev.vault.features.main_list.MainListEvent
+import com.arsvechkarev.vault.features.main_list.MainListEvent.ShowUsernamesChanged
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
+
+class ListenShowUsernamesActor(
+  private val showUsernamesInteractor: ShowUsernamesInteractor,
+) : Actor<MainListCommand, MainListEvent> {
+  
+  @OptIn(ExperimentalCoroutinesApi::class)
+  override fun handle(commands: Flow<MainListCommand>): Flow<MainListEvent> {
+    return showUsernamesInteractor.showUsernamesFlow
+        .mapLatest { ShowUsernamesChanged }
+  }
+}

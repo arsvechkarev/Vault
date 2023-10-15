6006d14416df822e210999c9902852f5d661e704
arsvechkarev <arsvechkarev@protonmail.com>
2023-10-15 13:21:13 +0400

    Implemented biometrics on login screen

--- android-app/app/src/main/java/com/arsvechkarev/vault/features/login/LoginDi.kt
+++ android-app/app/src/main/java/com/arsvechkarev/vault/features/login/LoginDi.kt
@@ -3,15 +3,25 @@ package com.arsvechkarev.vault.features.login
 import com.arsvechkarev.vault.core.mvi.tea.TeaStore
 import com.arsvechkarev.vault.core.mvi.tea.TeaStoreImpl
 import com.arsvechkarev.vault.features.common.di.CoreComponent
-import com.arsvechkarev.vault.features.login.actors.LoginActor
+import com.arsvechkarev.vault.features.login.actors.GetBiometricsEnterPossibleActor
 import com.arsvechkarev.vault.features.login.actors.LoginRouterActor
+import com.arsvechkarev.vault.features.login.actors.LoginWithBiometricsActor
+import com.arsvechkarev.vault.features.login.actors.LoginWithPasswordActor
 
 fun LoginStore(
   coreComponent: CoreComponent
-): TeaStore<LoginState, LoginUiEvent, Nothing> {
+): TeaStore<LoginState, LoginUiEvent, LoginNews> {
   return TeaStoreImpl(
     actors = listOf(
-      LoginActor(coreComponent.masterPasswordChecker),
+      GetBiometricsEnterPossibleActor(
+        coreComponent.biometricsEnabledProvider,
+        coreComponent.biometricsStorage
+      ),
+      LoginWithBiometricsActor(
+        coreComponent.biometricsStorage,
+        coreComponent.masterPasswordChecker
+      ),
+      LoginWithPasswordActor(coreComponent.masterPasswordChecker),
       LoginRouterActor(coreComponent.router)
     ),
     reducer = LoginReducer(),

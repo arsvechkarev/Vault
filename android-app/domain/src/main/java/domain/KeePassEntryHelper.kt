9e1eec440bd0bafa4c5435e9b3026b1ad4fd3de7
arsvechkarev <arsvechkarev@protonmail.com>
2023-10-22 21:38:04 +0400

    Added unit test for entries ui mapper

--- android-app/domain/src/main/java/domain/KeePassEntryHelper.kt
+++ android-app/domain/src/main/java/domain/KeePassEntryHelper.kt
@@ -3,17 +3,14 @@ package domain
 import app.keemobile.kotpass.models.Entry
 import domain.interactors.asBoolean
 
-val Entry.isDefinitePassword
-  get() = customData[CUSTOM_DATA_TYPE_KEY]?.value == CUSTOM_DATA_PASSWORD
-
 val Entry.isDefinitePlainText
   get() = customData[CUSTOM_DATA_TYPE_KEY]?.value == CUSTOM_DATA_PLAIN_TEXT
 
 val Entry.isProbablePlainText
   get() = fields.notes?.content?.isNotEmpty() == true
-      && fields.userName?.content?.isEmpty() == true
-      && fields.password?.content?.isEmpty() == true
-      && fields.url?.content?.isEmpty() == true
+      && fields.userName?.content.isNullOrEmpty()
+      && fields.password?.content.isNullOrEmpty()
+      && fields.url?.content.isNullOrEmpty()
 
 val Entry.isFavorite
   get() = customData[CUSTOM_DATA_FAVORITE_KEY]?.asBoolean() == true

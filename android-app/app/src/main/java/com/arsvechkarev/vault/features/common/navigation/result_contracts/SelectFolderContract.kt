2ab5925f81bf6b28a83efa47717ccc877d6b013f
arsvechkarev <arsvechkarev@protonmail.com>
2023-11-21 09:16:46 +0400

    Fixed initial backup folder opening

--- android-app/app/src/main/java/com/arsvechkarev/vault/features/common/navigation/result_contracts/SelectFolderContract.kt
+++ android-app/app/src/main/java/com/arsvechkarev/vault/features/common/navigation/result_contracts/SelectFolderContract.kt
@@ -3,12 +3,19 @@ package com.arsvechkarev.vault.features.common.navigation.result_contracts
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.provider.DocumentsContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.documentfile.provider.DocumentFile
 
 class SelectFolder : ActivityResultContracts.OpenDocumentTree() {
   
   override fun createIntent(context: Context, input: Uri?): Intent {
     return super.createIntent(context, input).apply {
+      if (input != null) {
+        DocumentFile.fromTreeUri(context, input)?.let { file ->
+          putExtra(DocumentsContract.EXTRA_INITIAL_URI, file.uri)
+        }
+      }
       addFlags(
         Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
             or Intent.FLAG_GRANT_WRITE_URI_PERMISSION

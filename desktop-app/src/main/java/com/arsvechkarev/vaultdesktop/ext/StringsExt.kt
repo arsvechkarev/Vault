85520ef27c52f37cad1559b27315d89e7f6cecf1
arsvechkarev <arsvechkarev@protonmail.com>
2023-11-22 18:29:42 +0400

    Removed obsolete desktop-app files

--- desktop-app/src/main/java/com/arsvechkarev/vaultdesktop/ext/StringsExt.kt
+++
@@ -1,9 +0,0 @@
-package com.arsvechkarev.vaultdesktop.ext
-
-import java.util.Locale
-
-fun String.firstCapitalizedLetter(): String {
-  return this[0].toString().replaceFirstChar {
-    if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
-  }
-}

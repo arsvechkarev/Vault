44d7740179b957687f26883d9777fd0d6df0444d
arsvechkarev <arsvechkarev@protonmail.com>
2023-12-06 14:36:00 +0400

    Bumped versionCode and versionName

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
     applicationId = "com.arsvechkarev.vault"
     minSdk = 26
     targetSdk = 34
-    versionCode = 1
-    versionName = "1.0.0"
+    versionCode = 2
+    versionName = "1.0.1"
     signingConfig = if (keystoreProperties.exists()) {
       signingConfigs.findByName("release")
     } else {

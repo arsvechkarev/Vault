cc5f3e21a1f2b3d9abc0962ff7f230c3f88e7878
arsvechkarev <arsvechkarev@protonmail.com>
2024-01-28 19:48:07 +0300

    Bumped version to 1.0.2

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
     applicationId = "com.arsvechkarev.vault"
     minSdk = 26
     targetSdk = 34
-    versionCode = 2
-    versionName = "1.0.1"
+    versionCode = 3
+    versionName = "1.0.2"
     signingConfig = if (keystoreProperties.exists()) {
       signingConfigs.findByName("release")
     } else {
